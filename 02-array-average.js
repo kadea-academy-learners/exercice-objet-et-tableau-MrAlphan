8a007f7eb2311b5569fe4331acf96974081d13e8
Alphan Wina wina <alphanisungu@gmail.com>
2025-10-30 16:16:33 +0100

    ajout de la corection de l'exercice 2

--- 02-array-average.js
+++ 02-array-average.js
@@ -2,10 +2,28 @@
 // Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
+// Crée un tableau contenant 5 notes
+let notes = [7, 14, 15, 9, 11];
 
 function averageNote(notes) {
-	
+
+  let somme = 0;
+  for (let i = 0; i < notes.length; i++) {
+    somme += notes[i];
+  }
+
+  let moyenne = somme / notes.length;
+
+  if (moyenne >= 10) {
+    return "Réussi (" + moyenne + ")";
+  } else {
+    return "Échoué (" + moyenne + ")";
+  }
 }
+
+
+console.log(averageNote(notes));
+
 
 module.exports = {
 	averageNote,
--- 03-user-management.js
+++ 03-user-management.js
@@ -5,9 +5,28 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
+// Fonction whoIsAdmin
+function whoIsAdmin(utilisateurs) {
+  
+  let admins = [];
+
+  for (let i = 0; i < utilisateurs.length; i++) {
+    if (utilisateurs[i].estAdmin === true) {
+      admins.push(utilisateurs[i].nom);
+    }
+  }
+
+  return admins;
 }
+
+let utilisateurs = [
+  { nom: "Carmel", age: 25, estAdmin: true },
+  { nom: "Exaucé", age: 21, estAdmin: true },
+  { nom: "Archange", age: 12, estAdmin: false },
+  { nom: "Gabriel", age: 10, estAdmin: false }
+];
+
+console.log(whoIsAdmin(utilisateurs));
 
 module.exports = {
 	whoIsAdmin,
